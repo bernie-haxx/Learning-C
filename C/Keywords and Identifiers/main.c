2af70a42a8d78145bffa9803cf86d23978410156
Ben Karanja <benkaranja43@gmail.com>
2018-12-03 13:10:41 +0300

    Learning on the variables and constants and also the various keywords in C

--- C/Keywords and Identifiers/main.c
+++ C/Keywords and Identifiers/main.c
@@ -2,6 +2,65 @@
 
 int main(int argc, char const *argv[])
 {
+	/*
+	Character set
+
+	This is a set of alphabets,letters and some special characters that are valid in C language
+	Alphabets
+	Uppercases and lower cases
+	C accepts both of them
+
+	And also Digits
+
+	And also special Characters like alphanumerics
+
+	Keywords
+
+	These are predefined words used in programmming that have special meanings to the compiler.
+	for example int money;
+	
+	Examples of the keywords:
+	auto
+	double
+	int 
+	struct
+	break
+	else 
+	long
+	switch
+	case
+	enum
+	register
+	typedef
+	char
+	extern
+	return
+	union
+	continue
+	for 
+	signed
+	void
+	do
+	if
+	static
+	while
+	default 
+	go
+	sizeof
+	volatile
+	const
+	float
+	short
+	unsigned
+
+	Identifiers
+	These are the namings based on variable representation
+	Rules::
+	1. A valid identifier can have letters(bith uppercase and lowercase letters, digits and underscores
+	2. The first letter of an identifier should be either a letter or an underscore.
+	3. There is no rule on how long an identifier can be. However, you may run into problems in some compilers if identifier is longer than 31 characters.)
+	 
+	*/
 	printf("%s\n", "red" );
 	return 0;
 }
---
+++ C/Variables/variable.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	/*
+	Rules fo naming a variable
+	1. Can have letters blah blah blah
+	2. The first letter can either be a letter or an underscore
+	3. There is no rule on how long a variable name can be. However, you may run into problems in some compilers if variable name is londer than 31 characters.
+	*/
+
+	/*
+	Constants/ Literals
+	This is a value whose value cannot be altered in a program
+	*/
+	// Integer
+	int playerScore = 95;
+
+	// Character
+	char ch = 'a';
+	ch = '1';
+	// constant
+	const double PI = 3.14;
+	printf("%s\n", "This is done my niggs/n" );
+	return 0;
+}
