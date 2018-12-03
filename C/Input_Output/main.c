7c31554bf97e3f37b94bef53622dba5b0afd7a5a
Ben Karanja <benkaranja43@gmail.com>
2018-12-03 15:06:28 +0300

    Learning on string formating and also input and output and using scanf and printf functions

---
+++ C/Input_Output/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h> // Including header to run printf("%s\n", );
+int main(int argc, char const *argv[])
+{
+	/* code */
+	printf("%s\n", "C Programming" );
+	// Integer Output
+
+	int testInteger = 5;
+	printf("Number = %d\n", testInteger );
+	
+	// Character I/O
+
+	char chr;
+	printf("Enter a character: ");
+	scanf("%c",&chr);
+	printf("You entered my %c\n", chr); 
+	return 0;
+
+}
